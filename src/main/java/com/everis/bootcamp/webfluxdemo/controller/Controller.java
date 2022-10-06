c2850c78a410dc0ee92048adc091f80784010ea2
alberto006-esp <albertosuarezolvera@gmail.com>
2022-10-06 20:34:54 +0200

    terminado webflux demo

--- src/main/java/com/everis/bootcamp/webfluxdemo/controller/Controller.java
+++ src/main/java/com/everis/bootcamp/webfluxdemo/controller/Controller.java
@@ -5,16 +5,28 @@ import java.time.Duration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.everis.bootcamp.webfluxdemo.repository.Subscriber;
+
 import reactor.core.publisher.Flux;
 
 @RestController
 public class Controller {
-	
-	@GetMapping(path="/numbers", produces="text/event-stream")
-	public Flux<Integer> numbers(){
-		
-		Flux<Integer> flux = Flux.range(1,30).delayElements(Duration.ofSeconds(1));
-		
+
+	@GetMapping(path = "/numbers", produces = "text/event-stream")
+	public Flux<Integer> numbers() {
+
+		Flux<Integer> flux = Flux.range(1, 30).delayElements(Duration.ofSeconds(1));
+
+		return flux;
+	}
+
+	@GetMapping(path = "/numbers-with-subscribers", produces = "text/event-stream")
+	public Flux<Integer> numbersWithSubscribers() {
+
+		Flux<Integer> flux = Flux.range(1, 30).delayElements(Duration.ofSeconds(1));
+
+		flux.subscribe(n->Subscriber.multiplicar(n)); // suscriptor 1
+		flux.subscribe(n->System.out.println("Subscriptor 2: " + n));// suscriptor 2
 		return flux;
 	}
 }
---
+++ src/main/java/com/everis/bootcamp/webfluxdemo/repository/Subscriber.java
@@ -0,0 +1,8 @@
+package com.everis.bootcamp.webfluxdemo.repository;
+
+public class Subscriber {
+
+    public static void multiplicar(Integer n)  {
+        System.out.println("Subscriber 1: "+n*n);
+    }
+}
